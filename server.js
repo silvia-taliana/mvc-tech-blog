daffb752f56d26bb94e13072166dae2979aabc6d
Silvia Taliana <silviataliana2@gmail.com>
2021-03-17 10:42:32 +1030

    setting up dependencies and connection

---
+++ server.js
@@ -0,0 +1,45 @@
+// getting dependencies and routes 
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+// const routes = require('./controllers'); will need when routes are set up
+// const helpers = require('./utils/helpers'); may need later 
+
+// setting up the connection with sequelize using the connection.js
+const sequelize = require('./config/connection');
+
+// connecting sequelize through session store 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({}); //if utils/helpers is created, insert const helpers here in curly brackets 
+
+// creating an object that stores information for the current session
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+// using the information stored in the current session 
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(routes); will need when routes are set up
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});

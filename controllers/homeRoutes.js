87b2546e61a9919371822c2d3a9b311304436d19
Silvia Taliana <silviataliana2@gmail.com>
2021-03-23 13:10:53 +1030

    adding blog post routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,73 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { BlogPost, User } = require('../models');
 const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+    try {
+        // Get all blog posts and JOIN with user data
+        const blogPostData = await BlogPost.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['title', 'post'],
+                },
+            ],
+        });
+
+        // Serialize data so the template can read it
+        const blogPosts = blogPostData.map((blogPost) => blogPost.get({ plain: true }));
+
+        // Pass serialized data and session flag into template
+        res.render('homepage', {
+            blogPosts,
+            loggedIn: req.session.loggedIn
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.get('/blogPost/:id', async (req, res) => {
+    try {
+        const blogPostData = await blogPost.findByPk(req.params.post_id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['title', 'post'],
+                },
+            ],
+        });
+
+        const blogPost = blogPostData.get({ plain: true });
+
+        res.render('blogPost', {
+            ...blogPost,
+            loggedIn: req.session.loggedIn
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        // Find the logged in user based on the session ID
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: { exclude: ['password'] },
+            include: [{ model: blogPost }],
+        });
+
+        const user = userData.get({ plain: true });
+
+        res.render('dashboard', {
+            ...user,
+            loggedIn: true
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 // Prevent non logged in users from viewing the homepage
 router.get('/', withAuth, async (req, res) => {
@@ -10,7 +77,7 @@ router.get('/', withAuth, async (req, res) => {
             order: [['name', 'ASC']],
         });
 
-        const users = userData.map((project) => project.get({ plain: true }));
+        const users = userData.map((blogPost) => blogPost.get({ plain: true }));
 
         res.render('homepage', {
             users,
--- models/User.js
+++ models/User.js
@@ -36,16 +36,16 @@ User.init(
     {
         hooks: {
             beforeCreate: async (newUserData) => {
-                console.log("hello");
                 newUserData.password = await bcrypt.hash(newUserData.password, 10);
                 return newUserData;
             },
-            beforeUpdate: async (newUserData) => {
-                newUserData.password = await bcrypt.hash(newUserData.password, 10);
-                return newUserData;
+            beforeUpdate: async (updatedUserData) => {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
             },
         },
         sequelize,
+        timestamps: false,
         freezeTableName: true,
         underscored: true,
         modelName: 'user',
